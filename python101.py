7d9574441e8b23f152cf53f9711ffa123457d216
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-13 14:26:48 -0500

    More python
    exit
    clear

--- python101.py
+++ python101.py
@@ -1,49 +1,81 @@
-print "Hello, World"
-print """
-It was a dark and stormy night.
-A murder happened
-"""
-print 'Hello, World'
-print 'Khanh "the man" Vu'
+# print "Hello, World"
+# print """
+# It was a dark and stormy night.
+# A murder happened
+# """
+# print 'Hello, World'
+# print 'Khanh "the man" Vu'
 
-# Variables
-# -strings, letters, numbers or any stuff with a keyboard
-# a variable is just a way to refer to something else
-# variables do not make the program faster, they make the program slower
-# variables make it easier for us to write programs
+# # Variables
+# # -strings, letters, numbers or any stuff with a keyboard
+# # a variable is just a way to refer to something else
+# # variables do not make the program faster, they make the program slower
+# # variables make it easier for us to write programs
 
-theBestClass = "the 11-18 immersive"
-print theBestClass
+# theBestClass = "the 11-18 immersive"
+# print theBestClass
 
-# Data Types:
-# - Programming languages see different types of variables differently
-# -string - english stuff
-# -numbers
-# floats, integers
-# -floats = it has a decimal in it 
-# -integer = has no decimal in it 
-# Booleans - true or false, off or on, 1 or 0, right or left, yes or no
-# list - a list of things, a single variable with a bunch of parts
-# dictionaries - a variable of variables
-# objects - super dictionaries
+# # Data Types:
+# # - Programming languages see different types of variables differently
+# # -string - english stuff
+# # -numbers
+# # floats, integers
+# # -floats = it has a decimal in it 
+# # -integer = has no decimal in it 
+# # Booleans - true or false, off or on, 1 or 0, right or left, yes or no
+# # list - a list of things, a single variable with a bunch of parts
+# # dictionaries - a variable of variables
+# # objects - super dictionaries
 
-# Primitive Date Types = string, number, Boolean
+# # Primitive Date Types = string, number, Boolean
 
-month = "November";
-print type(month);
+# month = "November";
+# print type(month);
 
-date = 13;
-print type(date);
+# date = 13;
+# print type(date);
 
-dateAsFloat = 13.0;
-print type(dateAsFloat);
+# dateAsFloat = 13.0;
+# print type(dateAsFloat);
 
-aBool = True;
-print type(aBool);
+# aBool = True;
+# print type(aBool);
 
-aList = [];
-print type(aList);
+# aList = [];
+# print type(aList);
 
-aDictionary = {};
-print type(aDictionary);
+# aDictionary = {};
+# print type(aDictionary);
+
+# # concatenate is programming speak for add things together
+# first = "Sean";
+# last = "McQuaid";
+# fullName = first + " " + last
+# print  fullName
+
+# fourteen = 10 + 4;
+# print fourteen
+
+# fourteen = "10" + "4";
+# print fourteen
+
+# # cast = change a variable to a new datetype
+# fourteen = int("10") + 4;
+# print fourteen
+
+# math  = +, -, /, *, %
+# % = modulus. Modulus divides the number and gives you the remainder
+# ** = exponent
+print 2 + 2
+print 2 - 2 
+print 2 / 2 
+print 2 * 2 
+print 2 % 2 
+print 2 ** 3 
+print "-" * 20 # gives you 20 of that particular string - give me x strings
+
+# python does not have a simple incrementer
+num = 1;
+# num++
+num += 1;
 
