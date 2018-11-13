85e98d73b5b02a1f1daa41b8dbad7b77beed4d58
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-13 12:03:07 -0500

    python practice

---
+++ python101.py
@@ -0,0 +1,30 @@
+print "Hello, World"
+print """
+It was a dark and stormy night.
+A murder happened
+"""
+print 'Hello, World'
+print 'Khanh "the man" Vu'
+
+# Variables
+# -strings, letters, numbers or any stuff with a keyboard
+# a variable is just a way to refer to something else
+# variables do not make the program faster, they make the program slower
+# variables make it easier for us to write programs
+
+theBestClass = "the 11-18 immersive"
+print theBestClass
+
+# Data Types:
+# - Programming languages see different types of variables differently
+# -string - english stuff
+# -numbers
+# floats, integers
+# -floats = it has a decimal in it 
+# -integer = has no decimal in it 
+# Booleans - true or false, off or on, 1 or 0, right or left, yes or no
+# list - a list of things, a single variable with a bunch of parts
+# dictionaries - a variable of variables
+# objects - super dictionaries
+
+# Primitive Date Types = string, number, Boolean
